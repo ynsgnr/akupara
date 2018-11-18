71d3002c997c52426ae4d485ff60aceed0e8499c
Yunus GÜngör <yunusgnr@gmail.com>
2018-11-18 19:25:31 +0300

    Added filters for door detection

--- src/openCV.py
+++ src/openCV.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
+#taken from http://wiki.ros.org/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
+
 import roslib
 roslib.load_manifest('akupara')
 import sys
@@ -9,9 +11,10 @@ import cv2
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
 
 consumes="/camera/rgb/image_raw"
-produces="/detection"
+produces="detection"
 
 class image_converter:
 
@@ -26,11 +29,44 @@ class image_converter:
     except CvBridgeError as e:
       print(e)
 
-    (rows,cols,channels) = cv_image.shape
-    if cols > 60 and rows > 60 :
-      cv2.circle(cv_image, (50,50), 10, 255)
+    #eliminate shadows
+    hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, (0, 0, 0), (0, 255,255))
+    rgbMask = cv2.cvtColor(mask,cv2.COLOR_GRAY2BGR)
+    shadowless = cv2.subtract(cv_image,rgbMask)
+
+    #detect shapes
+    gray = cv2.cvtColor(shadowless, cv2.COLOR_BGR2GRAY)
+    ret,thresh = cv2.threshold(gray,10,255,1)
+    inverted=cv2.bitwise_not(thresh)
+    blurred = cv2.dilate(inverted,np.ones((5, 5), np.uint8),iterations = 2 )
+    image, contours, h = cv2.findContours(blurred,1,2)
+    contouredImg=cv_image.copy()
+
+    for cnt in contours:
+        approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+        print("Detected shape has "+str(len(approx))+" sides and it is:")
+        if len(approx)==5:
+            print ("pentagon")
+            cv2.drawContours(contouredImg,[cnt],0,255,-1)
+        elif len(approx)==3:
+            print ("triangle")
+            cv2.drawContours(contouredImg,[cnt],0,(0,255,0),-1)
+        elif len(approx)==4:
+            print ("square")
+            cv2.drawContours(contouredImg,[cnt],0,(0,0,255),-1)
+        elif len(approx) == 9:
+            print ("half-circle")
+            cv2.drawContours(contouredImg,[cnt],0,(255,255,0),-1)
+        elif len(approx) > 15:
+            print ("circle")
+            cv2.drawContours(contouredImg,[cnt],0,(0,255,255),-1)
+
+
+    edges = cv2.Canny(shadowless,100,200)
 
     cv2.imshow("Image window", cv_image)
+    cv2.imshow("Shapes window", contouredImg)
     cv2.waitKey(3)
 
     try:
