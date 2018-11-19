408b23b4dc8ce62267938083abae3b9cc717cf2e
Yunus GÜngör <yunusgnr@gmail.com>
2018-11-20 01:05:01 +0300

    Optimized mean

--- src/openCV.py
+++ src/openCV.py
@@ -31,6 +31,8 @@ class image_converter:
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber(consumes,Image,self.callback)
     self.motor_command_publisher = rospy.Publisher("/cmd_vel_mux/input/navi", Twist, queue_size=100)
+    self.depth_sub = rospy.Subscriber("/camera/depth/image_raw",Image,self.depth_callback)
+    self.depth_image=np.zeros((640,480))
 
   def callback(self,data):
     try:
@@ -66,25 +68,38 @@ class image_converter:
     edges = cv2.Canny(gray,100,200)
     image, contours, h = cv2.findContours(edges,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     edgeCountered=cv_image.copy()
-    cmask = np.zeros(gray.shape,np.uint8)
     for cnt in contours:
+        cmask = np.zeros(gray.shape,np.uint8)
         approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-        cv2.drawContours(cmask,[cnt],0,255,-1) #draw on mask
+        cv2.drawContours(cmask,[cnt],0,255,cv2.FILLED) #draw on mask
         c=cv2.mean(cv_image,cmask)
         l=len(approx)
         a=0
         t=0
+        #Get central coordinates:
+        x=0
+        y=0
+        for v in cnt:
+            x=x+v[0][0]
+            y=y+v[0][1]
+        x=x/len(cnt)
+        y=y/len(cnt)
         if l<8 and c[0]>20 and c[1]>20 and c[2]>20:
             a=cv2.contourArea(cnt)
-            if a>0:
-                t=1
-        cv2.drawContours(edgeCountered,[cnt],0,c,cv2.FILLED) #draw avarage color on img
-        doors.append([l,a,c,t])
+            t=1
+            cv2.drawContours(edgeCountered,[cnt],0,(255-c[0],255-c[1],255-c[2]),cv2.FILLED) #draw avarage color on img
+        doors.append([l,a,c,t,(x,y)])
 
-    print("Detected "+str(len(doors))+" shapes")
+    #print("Detected "+str(len(doors))+" shapes")
     for d in doors:
         if(d[3]>0):
-            print("Has "+str(d[0])+" lines and "+str(d[1])+" area and color "+str(d[2]))
+            if(x>2*(len(cv_image)/3)):
+                where="left"
+            elif(x>len(cv_image)/3):
+                where="mid"
+            else:
+                where="right"
+            #print("Has "+str(d[0])+" lines and "+str(d[1])+" area and color "+str(d[2])+" its on the "+where)
 
     # h, w = edges.shape[:2]
     # maskFill = np.zeros((h+2, w+2), np.uint8)
@@ -95,21 +110,49 @@ class image_converter:
     cv2.imshow("Image window", cv_image)
     cv2.imshow("Shapes by edge window", edgeCountered)
     cv2.imshow("Edges window", edges)
-    cv2.imshow("Doors window", cmask)
+    cv2.imshow("Depth window", self.depth_image)
+
+    #Movement Part
+    step=0.3
+    threshold=0.3
+    motor_command=Twist()
+
+    #calculate mean
+    l=len(self.depth_image)
+    self.depth_image[~np.isfinite(self.depth_image)]=-1 #replace NaN and inf
+    ml=cv2.mean(self.depth_image[:l/2,:])[0]
+    mr=cv2.mean(self.depth_image[l/2:,:])[0]
+    m=cv2.mean(self.depth_image)[0]
+    print(m)
+    print(ml)
+    print(mr)
+    if(m<threshold):
+        print("Too close to the wall, follow wall")
+    if(ml<threshold):
+        print("Too close to left wall, go right")
+    if(mr<threshold):
+        print("Too close to right wall, go left")
 
 
     cv2.waitKey(3)
 
     #move
-    motor_command=Twist()
-    motor_command.linear.x=0
     motor_command.angular.z=0
+
+
     self.motor_command_publisher.publish(motor_command)
 
     try:
       self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
     except CvBridgeError as e:
       print(e)
+
+  def depth_callback(self,msg_depth):
+      try:
+          self.depth_image = self.bridge.imgmsg_to_cv2(msg_depth, "32FC1")
+      except CvBridgeError as e:
+         print(e)
+
 
 def main(args):
   ic = image_converter()
